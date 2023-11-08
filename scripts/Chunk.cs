36171d917c1dc48823e02267cf53e2f85fb79503
laumi73 <laumichael73@gmail.com>
2023-11-08 00:32:47 -0500

    Drew 1 face (top)

--- scripts/Chunk.cs
+++ scripts/Chunk.cs
@@ -4,47 +4,49 @@ using System.Collections.Generic;
 
 public partial class Chunk : Node3D
 {
-	List<Vector3> vertices = new();
-	List<int> triangles = new();
-
-	private Mesh _mesh;
+	private ArrayMesh _arrayMesh;
 	private MeshInstance3D _meshInstance3D;
 	private SurfaceTool _surfaceTool = new();
-
-
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_arrayMesh = new();
+		_AddMeshInstance();
+
+		_surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
+		DrawFace();
+		_surfaceTool.GenerateNormals(false);
+		_surfaceTool.Commit(_arrayMesh);
+
+		_meshInstance3D.Mesh = _arrayMesh;
+	}
+
+	internal void DrawFace() {
+		Vector3[] vertices = new Vector3[] {
+			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 0]],
+			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 1]],
+			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 2]],
+			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 3]]
+		};
+
+		Vector3[] triangle1 = new Vector3[] {vertices[0], vertices[1], vertices[2]};
+		Vector3[] triangle2 = new Vector3[] {vertices[2], vertices[1], vertices[3]};
+
+		_surfaceTool.AddTriangleFan(triangle1);
+		_surfaceTool.AddTriangleFan(triangle2);
+
+		//_surfaceTool.AddTriangleFan(new Vector3[] {new(0, 1, 0), new(1, 1, 0), new(1, 1, 1)});
+	}
+
+	private void _AddMeshInstance() {
 		if (_meshInstance3D != null) {
 			_meshInstance3D.CallDeferred("queue_free");
 			_meshInstance3D = null;
 		}
-		_mesh = new();
+	
 		_meshInstance3D = new();
-
-
-		DrawFace();
-	}
-
-	internal void DrawFace() {
-		vertices.Add(VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 0]]);
-		vertices.Add(VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 1]]);
-		vertices.Add(VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 2]]);
-		vertices.Add(VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 2]]);
-		vertices.Add(VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 1]]);
-		vertices.Add(VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 3]]);
-
-		/*
-		vertices = new List<Vector3>(6) {
-			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 0]],
-			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 1]],
-			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 2]],
-			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 2]],
-			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 1]],
-			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 3]]
-		};
-		*/
+		AddChild(_meshInstance3D);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/VoxelData.cs
+++ scripts/VoxelData.cs
@@ -3,17 +3,17 @@ using Godot;
 public static class VoxelData
 {
 	public static readonly Vector3[] voxelVertices = new Vector3[8] {
-		new(0f, 0f, 0f),
-		new(1f, 0f, 0f),
-		new(1f, 1f, 0f),
-		new(0f, 1f, 0f),
-		new(0f, 0f, 1f),
-		new(1f, 0f, 1f),
-		new(1f, 1f, 1f),
-		new(0f, 1f, 1f)
+		new(0f, 0f, 0f), //0
+		new(1f, 0f, 0f), //1
+		new(1f, 1f, 0f), //2
+		new(0f, 1f, 0f), //3
+		new(0f, 0f, 1f), //4
+		new(1f, 0f, 1f), //5
+		new(1f, 1f, 1f), //6
+		new(0f, 1f, 1f)  //7
 	};
 
 	public static readonly int[,] voxelTriangles = new int[1,4] {
-		{3, 7, 2, 6} // Top face
+		{2, 6, 3, 7} // Top face
 	};
 }
