df804e42c08d5f4302b432a60f8c993cb459910f
laumi73 <laumichael73@gmail.com>
2023-11-12 18:50:55 -0500

    Formatting

--- scripts/Chunk.cs
+++ scripts/Chunk.cs
@@ -1,56 +1,51 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
 
-public partial class Chunk : Node3D
+namespace GameSpace
 {
-	private ArrayMesh _arrayMesh;
-	private MeshInstance3D _meshInstance3D;
-	private SurfaceTool _surfaceTool = new();
+    public partial class Chunk : Node3D
+    {
+        private ArrayMesh _arrayMesh;
+        private MeshInstance3D _meshInstance3D;
+        private SurfaceTool _surfaceTool = new();
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		_arrayMesh = new();
-		_AddMeshInstance();
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready()
+        {
+            _meshInstance3D = new MeshInstance3D();
+            _arrayMesh = new();
 
-		_surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
-		DrawFace();
-		_surfaceTool.GenerateNormals(false);
-		_surfaceTool.Commit(_arrayMesh);
+            _surfaceTool.Begin(Mesh.PrimitiveType.Triangles);
+            DrawFaces();
+            _surfaceTool.GenerateNormals(false);
+            _surfaceTool.Commit(_arrayMesh);
 
-		_meshInstance3D.Mesh = _arrayMesh;
-	}
+            _meshInstance3D.Mesh = _arrayMesh;
 
-	internal void DrawFace() {
-		Vector3[] vertices = new Vector3[] {
-			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 0]],
-			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 1]],
-			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 2]],
-			VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 3]]
-		};
+            AddChild(_meshInstance3D);
+        }
 
-		Vector3[] triangle1 = new Vector3[] {vertices[0], vertices[1], vertices[2]};
-		Vector3[] triangle2 = new Vector3[] {vertices[2], vertices[1], vertices[3]};
+        internal void DrawFaces()
+        {
+            Vector3[] vertices = new Vector3[] {
+                VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 0]],
+                VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 1]],
+                VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 2]],
+                VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 3]]
+            };
 
-		_surfaceTool.AddTriangleFan(triangle1);
-		_surfaceTool.AddTriangleFan(triangle2);
+            Vector3[] triangle1 = new Vector3[] { vertices[0], vertices[1], vertices[2] };
+            Vector3[] triangle2 = new Vector3[] { vertices[2], vertices[1], vertices[3] };
 
-		//_surfaceTool.AddTriangleFan(new Vector3[] {new(0, 1, 0), new(1, 1, 0), new(1, 1, 1)});
-	}
+            _surfaceTool.AddTriangleFan(triangle1);
+            _surfaceTool.AddTriangleFan(triangle2);
+        }
 
-	private void _AddMeshInstance() {
-		if (_meshInstance3D != null) {
-			_meshInstance3D.CallDeferred("queue_free");
-			_meshInstance3D = null;
-		}
-	
-		_meshInstance3D = new();
-		AddChild(_meshInstance3D);
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(double delta)
+        {
+        }
+    }
 }
