7b617d5393f248510d9c1371a543349ab3defb37
laumi73 <laumichael73@gmail.com>
2023-11-12 18:51:40 -0500

    Organized gamespace-related classes into namespace

--- scripts/VoxelData.cs
+++ scripts/VoxelData.cs
@@ -1,9 +1,11 @@
 using Godot;
 
-public static class VoxelData
+namespace GameSpace
 {
-	public static readonly Vector3[] voxelVertices = new Vector3[8] {
-		new(0f, 0f, 0f), //0
+    public static class VoxelData
+    {
+        public static readonly Vector3[] voxelVertices = new Vector3[8] {
+        new(0f, 0f, 0f), //0
 		new(1f, 0f, 0f), //1
 		new(1f, 1f, 0f), //2
 		new(0f, 1f, 0f), //3
@@ -13,12 +15,13 @@ public static class VoxelData
 		new(0f, 1f, 1f)  //7
 	};
 
-	public static readonly byte[,] voxelTriangles = new byte[6,4] {
-		{2, 6, 3, 7}, // Top face
+        public static readonly byte[,] voxelTriangles = new byte[6, 4] {
+        {2, 6, 3, 7}, // Top face
 		{0, 4, 1, 5}, // Bottom face
 		{0, 3, 4, 7}, // Left face
 		{5, 6, 1, 2}, // Right face
 		{4, 7, 5, 6}, // Front face
 		{1, 2, 0, 3}  // Back face
 	};
+    }
 }
