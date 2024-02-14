966612351da7056cf92f9c7f6914d670d9dffe08
laumi73 <laumichael73@gmail.com>
2024-02-14 00:57:40 -0500

    Added scripts for drawing all 6 faces of a block

--- scripts/Chunk.cs
+++ scripts/Chunk.cs
@@ -14,6 +14,17 @@ namespace GameSpace
         // Called when the node enters the scene tree for the first time.
         public override void _Ready()
         {
+            base._Ready();
+            DrawBlock();
+        }
+
+        // Called every frame. 'delta' is the elapsed time since the previous frame.
+        public override void _Process(double delta)
+        {
+            base._Process(delta);
+        }
+
+        private void DrawBlock() {
             _meshInstance3D = new MeshInstance3D();
             _arrayMesh = new();
 
@@ -22,18 +33,29 @@ namespace GameSpace
             _surfaceTool.GenerateNormals(false);
             _surfaceTool.Commit(_arrayMesh);
 
+
             _meshInstance3D.Mesh = _arrayMesh;
+            _meshInstance3D.CreateTrimeshCollision();
 
             AddChild(_meshInstance3D);
         }
 
-        internal void DrawFaces()
+        private void DrawFaces()
         {
+            DrawFaceByDirection(Convert.ToByte(VoxelData.FaceIndex.TOP));
+            DrawFaceByDirection(Convert.ToByte(VoxelData.FaceIndex.BOTTOM));
+            DrawFaceByDirection(Convert.ToByte(VoxelData.FaceIndex.LEFT));
+            DrawFaceByDirection(Convert.ToByte(VoxelData.FaceIndex.RIGHT));
+            DrawFaceByDirection(Convert.ToByte(VoxelData.FaceIndex.FRONT));
+            DrawFaceByDirection(Convert.ToByte(VoxelData.FaceIndex.BACK));
+        }
+
+        private void DrawFaceByDirection(byte directionIndex) {
             Vector3[] vertices = new Vector3[] {
-                VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 0]],
-                VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 1]],
-                VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 2]],
-                VoxelData.voxelVertices[VoxelData.voxelTriangles[0, 3]]
+                VoxelData.voxelVertices[VoxelData.voxelTriangles[directionIndex, 0]],
+                VoxelData.voxelVertices[VoxelData.voxelTriangles[directionIndex, 1]],
+                VoxelData.voxelVertices[VoxelData.voxelTriangles[directionIndex, 2]],
+                VoxelData.voxelVertices[VoxelData.voxelTriangles[directionIndex, 3]]
             };
 
             Vector3[] triangle1 = new Vector3[] { vertices[0], vertices[1], vertices[2] };
@@ -41,11 +63,6 @@ namespace GameSpace
 
             _surfaceTool.AddTriangleFan(triangle1);
             _surfaceTool.AddTriangleFan(triangle2);
-        }
-
-        // Called every frame. 'delta' is the elapsed time since the previous frame.
-        public override void _Process(double delta)
-        {
         }
     }
 }
--- scripts/VoxelData.cs
+++ scripts/VoxelData.cs
@@ -5,23 +5,32 @@ namespace GameSpace
     public static class VoxelData
     {
         public static readonly Vector3[] voxelVertices = new Vector3[8] {
-        new(0f, 0f, 0f), //0
-		new(1f, 0f, 0f), //1
-		new(1f, 1f, 0f), //2
-		new(0f, 1f, 0f), //3
-		new(0f, 0f, 1f), //4
-		new(1f, 0f, 1f), //5
-		new(1f, 1f, 1f), //6
-		new(0f, 1f, 1f)  //7
-	};
+			new(0f, 0f, 0f), //0
+			new(1f, 0f, 0f), //1
+			new(1f, 1f, 0f), //2
+			new(0f, 1f, 0f), //3
+			new(0f, 0f, 1f), //4
+			new(1f, 0f, 1f), //5
+			new(1f, 1f, 1f), //6
+			new(0f, 1f, 1f)  //7
+		};
 
         public static readonly byte[,] voxelTriangles = new byte[6, 4] {
-        {2, 6, 3, 7}, // Top face
-		{0, 4, 1, 5}, // Bottom face
-		{0, 3, 4, 7}, // Left face
-		{5, 6, 1, 2}, // Right face
-		{4, 7, 5, 6}, // Front face
-		{1, 2, 0, 3}  // Back face
-	};
+			{2, 6, 3, 7}, // Top face
+			{0, 4, 1, 5}, // Bottom face
+			{0, 3, 4, 7}, // Left face
+			{5, 6, 1, 2}, // Right face
+			{4, 7, 5, 6}, // Front face
+			{1, 2, 0, 3}  // Back face
+		};
+
+		public enum FaceIndex {
+			TOP = 0,
+			BOTTOM = 1,
+			LEFT = 2,
+			RIGHT = 3,
+			FRONT = 4,
+			BACK = 5
+		}
     }
 }
